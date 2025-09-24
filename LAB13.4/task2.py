846ea172569021fe39d2b6e5a0d202cef7869428
2403a51004 <2403a51004@sru.edu.in>
2025-09-24 21:54:40 +0530

    Add files via upload

---
+++ LAB13.4/task2.py
@@ -0,0 +1,11 @@
+
+def make_sentence(words):
+    sentence = ""
+    for word in words:
+        sentence += word + " "
+    return sentence.strip()  # remove trailing space
+
+# Example usage
+words = ["AI", "helps", "in", "refactoring", "code"]
+result = make_sentence(words)
+print(result)
